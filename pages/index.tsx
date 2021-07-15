90a0a9601c9af9df15e9334bfe9ab854bad71c18
shotakamei1023 <kameishota1023@gmail.com>
2021-07-16 02:49:33 +0900

    ヘッダー（SP）完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
 import Head from 'next/head'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
+import Image from 'next/image'
+
 
 export default function Home({ allPostsData }) {
   return (
-    <div>
+    <div className="font-Lato">
       <Head>
         <title>MyProfile</title>
       </Head>
-      <section>
-
-      </section>
-    </div>
+      <header className="flex flex-wrap max-w-78vw mx-auto">
+        <div className="w-full">
+          <img src="/logo.png" alt="プロフィール画像" loading="lazy" className="max-w-72 mx-auto" />
+        </div>
+        <nav className="w-78vw">
+          <ul className="text-xl font-bold flex justify-between">
+            <li>about</li>
+            <li>skills</li>
+            <li>values</li>
+            <li>future</li>
+          </ul>
+        </nav>
+      </header >
+    </div >
   )
 }
 
